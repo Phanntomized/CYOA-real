c5929901f83739f2bc6f9e0612d3c56685c2dc48
Atticus P. Boon <boona@wlhs.wlwv.k12.or.us>
2024-10-30 16:55:10 +0000

    fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-
+import random
 
 # Credit to ChatGPT for delayed letter code
 text_delay = 0.001
@@ -154,36 +154,134 @@ if x == "x":
 else:
        intro()
 if start:
-      time.sleep(2)
-      print_with_delay(Colors.GREEN + "You live in a run down apartment in one of the worst parts of town.")
-      time.sleep(2)
-      print_with_delay("You need money.")
-      time.sleep(2)
-      print_with_delay("One day while wandering through a junk shop in search of cheap furniture, you see something shining between two pieces of junk.")
-      time.sleep(2)
-      print_with_delay("You pull it out and you discover that it's' a golden toaster.")
-      time.sleep(2)
-      print_with_delay("It looks a little strange though, because instead of toasting settings on the knob, all it says is 'Past', 'Present', and 'Future'.")
-      time.sleep(2)
-      print_with_delay(Colors.BLUE + "Interesting, I think I'll buy this as a gag gift.")
-      time.sleep(2)
-      print_with_delay(Colors.GREEN + "Later when you get home, you decide to plug in the toaster to try to toast a slice of bread.")
-      time.sleep(2)
-      print_with_delay("When you plug in the toaster and insert a pice fo bread, you hear a strange voice start talking to you inside of you head.")
-      time.sleep(2)
-      print_with_delay(Colors.RED + "Hey kid, want to earn some sweet dough?")
-      time.sleep(2)
-      print_with_delay(Colors.BLUE + "I-I'm not sure, who are you and why are you talking to me?")
-      time.sleep(2)
-      print_with_delay(Colors.RED + "Come on kid, you know it's me, your toaster talking to you. And also I'm not a toaster, I'm a time travel machine. I can take you anywhere in time and you could become rich!")
-      time.sleep(2)
-      ans1 = input(Colors.MAGENTA + "Accept or decline the toaster's offer? ")
+       clay_statue = False
 
-      if ans1 == "Decline":
-            time.sleep(2)
-            print_with_delay(Colors.BLUE + "I don't believe you.")
-            time.sleep(2)
-            print_with_delay(Colors.RED + "Well then let me show you...")
-            time.sleep(2)
-            print_with_delay(Colors.YELLOW + "Zroom!")
-            time.sleep(2)
+       time.sleep(2)
+       print_with_delay(Colors.GREEN + "You live in a run down apartment in one of the worst parts of town.")
+       time.sleep(2)
+       print_with_delay("You need money.")
+       time.sleep(2)
+       print_with_delay("One day while wandering through a junk shop in search of cheap furniture, you see something shining between two pieces of junk.")
+       time.sleep(2)
+       print_with_delay("You pull it out and you discover that it's' a golden toaster.")
+       time.sleep(2)
+       print_with_delay("It looks a little strange though, because instead of toasting settings on the knob, all it says is 'Past', 'Present', and 'Future'.")
+       time.sleep(2)
+       print_with_delay(Colors.BLUE + "Interesting, I think I'll buy this as a gag gift.")
+       time.sleep(2)
+       print_with_delay(Colors.GREEN + "Later when you get home, you decide to plug in the toaster to try to toast a slice of bread.")
+       time.sleep(2)
+       print_with_delay("When you plug in the toaster and insert a pice fo bread, you hear a strange voice start talking to you inside of you head.")
+       time.sleep(2)
+       print_with_delay(Colors.RED + "Hey kid, want to earn some sweet dough?")
+       time.sleep(2)
+       print_with_delay(Colors.BLUE + "I-I'm not sure, who are you and why are you talking to me?")
+       time.sleep(2)
+       print_with_delay(Colors.RED + "Come on kid, you know it's me, your toaster talking to you. And also I'm not a toaster, I'm a time travel machine. I can take you anywhere in time and you could become rich!")
+       time.sleep(2)
+       ans1 = input(Colors.MAGENTA + "Accept or decline the toaster's offer? (yes/no) ")
+       time.sleep(2)
+
+       if ans1 == "Decline":
+              print_with_delay(Colors.BLUE + "I don't believe you.")
+              time.sleep(2)
+              print_with_delay(Colors.RED + "Well then let me show you...")
+              time.sleep(2)
+              print_with_delay(Colors.YELLOW + "Zroom!")
+              time.sleep(2)
+              world = random.randint(1,6)
+            
+       if world == 1:
+              print_with_delay(Colors.GREEN + "You find yourself in a cave with some fresh cave paintings.")
+              time.sleep(2)
+              print_with_delay(Colors.BLUE + "Uh oh. That toaster must have been telling the truth.")
+              time.sleep(2)
+              print_with_delay(Colors.GREEN + "Speaking of the toaster, it's nowhere to be seen.")
+              time.sleep(2)
+              print_with_delay(Colors.BLUE + "I should probably look for that toaster, so it can take me back.")
+              time.sleep(2)
+              world1ans1 = (Colors.MAGENTA + "Explore inside or explore outside? (inside/outside) ")
+              time.sleep(2)
+
+       if world1ans1 == "inside":
+              print_with_delay(Colors.GREEN + "You decide to explore the cave.")
+              time.sleep(2)
+              print_with_delay(Colors.WHITE + "You find a small, clay statue.")
+              clay_statue = True
+              time.sleep(2)
+              print_with_delay("As you investigate the caves you realize you must be somewhere in the stone age.")
+              time.sleep(2)
+       elif world1ans1 == "outside":
+              print_with_delay(Colors.GREEN + "You decide to explore outside the cave.")
+              time.sleep(2)
+              print_with_delay("It's hard to tell what time period you got warped into, because there's no sign of civilization apart from the cave.")
+              time.sleep(2)
+       
+       print_with_delay("You notice two people that kind of look like neanderthals coming towards you.")
+       time.sleep(2)
+       print_with_delay("They are wearing crude sheepskins.")
+       time.sleep(2)
+       if world1ans1 == "outside":
+             print_with_delay("You realize that they are neanderthals and you must be somewhere in the stone age.")
+             time.sleep(2)
+       print_with_delay("The as they get closer, you notice they don't look happy that your in their territory.")
+       time.sleep(2)
+       world1ans2 = input(Colors.MAGENTA + "Run away or face the neanderthals? (run/stay) ")
+       time.sleep(2)
+       
+       if world1ans2 == "run":
+             print_with_delay(Colors.GREEN + "You decide to run away.")
+             time.sleep(2)
+             print_with_delay("Uh oh. The neanderthals decided to chase after you, and they have clubs!")
+             time.sleep(2)
+             print_with_delay("You run until you get to a waterfall.")
+             time.sleep(2)
+             print_with_delay("The neanderthals are still chasing you.")
+             time.sleep(2)
+             world1ans3 = input(Colors.MAGENTA + "Jump down the waterfall or face he neanderthals? (jump/stay)")
+             time.sleep(2)
+       if world1ans2 or world1ans3 == "stay":
+             print_with_delay(Colors.GREEN + "You decide to stay and face the neanderthals.")
+             time.sleep(2)
+             print_with_delay("They approach you and make it clear they want you to give them something.")
+             time.sleep(2)
+             if clay_statue:
+                   print_with_delay("You remember you still have the clay statue you took from the cave.")
+                   time.sleep(2)
+                   print_with_delay(Colors.BLUE + "If I give them the statue, it may appease them...")
+                   time.sleep(2)
+                   print_with_delay("...But if I ever do get back to my time, I could probably sell it for a lot of money.")
+                   time.sleep(2)
+                   world1ans4 = input(Colors.MAGENTA + "Give the statue or keep it? (give/keep) ")
+                   time.sleep(2)
+                   if world1ans4 == "give":
+                         print_with_delay(Colors.GREEN + "You decide to give the neanderthals the clay statue.")
+                         time.sleep(2)
+                         print_with_delay("...One of them takes the statue and lokks at it, then the one holding the statue walks away.")
+                         time.sleep(2)
+                         print_with_delay("The other one doesn't walk away but makes it clear that they want a statue as well.")
+                         time.sleep(2)
+                         print_with_delay("Now that there's only one, you might be able to take them in a fight.")
+                         time.sleep(2)
+                         world1ans5 = input(Colors.MAGENTA + "Fight the neanderthal or shrug apologetically? (fight/shrug) ")
+                         time.sleep(2)
+                         if world1ans5 == "fight":
+                               print_with_delay(Colors.GREEN + "You decide to try to fight the neanderthal.")
+                               time.sleep(2)
+                               neanderthal_health = 100
+                               print_with_delay("You throw a punch at the neanderthal.")
+                               time.sleep(2)
+                               swing = random.randint(1,3)
+                               if swing == 2:
+                                   print_with_delay("The blow dazes the neanderthal, but only for a second.")
+                                   neanderthal_health -= 10
+                               elif swing == 3:
+                                   print_with_delay("The blow catches the neanderthal off guard and knocks them down...")
+                                   neanderthal_health -= 20
+                               time.sleep(2)
+                               print_with_delay("Now its the neanderthal's turn to take a swing at you.")
+                               time.sleep(2)
+                               print_with_delay("You must predict where the neanderthal is going to swing so you can dodge it.")
+                               time.sleep(2)
+                               swing = random.randint(1,2)
+                               world1ans6 = input("Will the neanderthal swing left or right? (left/right) ")
