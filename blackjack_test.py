df7f7ed2faef0e8da7b954c3ff69b4a121e1d567
Atticus P. Boon <boona@wlhs.wlwv.k12.or.us>
2024-11-24 05:03:55 +0000

    blackjack tests

--- blackjack_test.py
+++ blackjack_test.py
@@ -7,7 +7,10 @@ def blackjack():
               bust = 0
               round = 1
               print("Round 1")
-              while new_game != True:
+              time.sleep(3)
+              while new_game == False:
+                     ace = False
+                     round_over = False
                      num = random.randint(2,11)
                      name1 = num
                      num2 = random.randint(2,11)
@@ -15,7 +18,7 @@ def blackjack():
                      total = num + num2
                      if num == 11:
                             name = "an ace"
-                     elif num2 == 11:
+                     if num2 == 11:
                             name2 = "an ace"
 
                      opnum = random.randint(2,11)
@@ -25,15 +28,15 @@ def blackjack():
                      optotal = opnum + opnum2
                      if opnum == 11:
                             opname = "an ace"
-                     elif opnum2 == 11:
+                     if opnum2 == 11:
                             opname2 = "an ace"
                      print("You get dealt 2 cards.")
                      time.sleep(3)
-                     print(f"Your card's values are {name1} and {name2}, and your total is {total}.")
+                     print(f"Your card's values are {name1} and {name2}.")
                      time.sleep(3)
                      print(f"Your opponent has {opname} and {opname2}.")
                      time.sleep(3)
-                     while round_over != True:
+                     while round_over == False:
                             hit_stand = input("Hit or stand? (hit/stand) ")
                             time.sleep(1)
                             if hit_stand == "hit":
@@ -41,35 +44,45 @@ def blackjack():
                                    total += num
                                    if num == 11:
                                           name = "an ace"
+                                          ace = True
                                    else:
                                           name = False
-                                   print(f"You drew {name}.")
+                                   if name == "an ace":
+                                          print(f"You drew {name}.")
+                                   else:
+                                          print(f"You drew {num}")
                                    time.sleep(3)
-                                   if name == "an ace" and total > 21:
+                                   if ace and total > 21:
                                           total -= 10
                                           print(f"Your ace's value changes to 1 and your total is {total}.")
                                           time.sleep(3)
-                                          if total <= 21:
-                                                 continue
+                                   if total <= 21:
+                                          continue
                                    if total > 21:
                                           print("You busted!")
                                           time.sleep(3)
                                           bust += 1
                                           round += 1
                                           round_over = True
+                                          break
                             else:
                                    print(f"You stood with {total}.")
                                    time.sleep(3)
 
+                            opace = False
                             opnum = random.randint(2,11)
                             optotal += opnum
                             if opnum == 11:
                                    opname = "an ace"
+                                   opace = True
                             else:
                                    opname = False
-                            print(f"Your opponent draws {opnum}.")
+                            if opname == "an ace":
+                                   print(f"Your opponent draws {opname}")
+                            else:
+                                   print(f"Your opponent draws {opnum}.")
                             time.sleep(3)
-                            if opname == "an ace" and optotal > 21:
+                            if opace and optotal > 21:
                                    optotal -= 10
                                    print(f"Their ace's value changes to 1 and their total is {optotal}.")
                                    time.sleep(3)
@@ -98,7 +111,8 @@ def blackjack():
                             time.sleep(3)
                             continue
                      else:
-                            print("You lost and were thrown out fo the casino.")
+                            print("You lost and were thrown out of the casino.")
                             time.sleep(3)
                             return "lost"
 
+blackjack()
