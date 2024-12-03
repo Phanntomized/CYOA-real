5298cb9f63e7e166f13cc74584a8518eaf4076d5
Atticus P. Boon <boona@wlhs.wlwv.k12.or.us>
2024-12-03 18:06:45 +0000

    v0.6

--- main.py
+++ main.py
@@ -382,7 +382,6 @@ def world2():
               round = 1
               opdone = False
               card_num = 2
-              suspect = 0
               time.sleep(3)
               while new_game == False:
                      card_num = 2
@@ -391,7 +390,7 @@ def world2():
                      optotal = 0
                      ace = 0
                      opace = 0
-                     print(f"Round {round}")
+                     print_with_delay(Colors.WHITE + f"Round {round}")
                      time.sleep(3)
                      round += 1
                      round_over = False
@@ -419,25 +418,25 @@ def world2():
                             opname2 = "an ace"
                             opace += 1
                      optotal = opnum + opnum2
-                     print("You each get dealt 2 cards.")
+                     print_with_delay(Colors.GREEN + "You each get dealt 2 cards.")
                      time.sleep(3)
-                     print(f"Your card's values are {name} and {name2}.")
+                     print_with_delay(f"Your card's values are {name} and {name2}.")
                      time.sleep(3)
                      if ace > 0 and total > 21:
                             total -= 10
-                            print(f"Your ace's value changes to 1 and your total is {total}.")
+                            print_with_delay(f"Your ace's value changes to 1 and your total is {total}.")
                             name = False
                             time.sleep(3)
-                     print(f"Your opponent has {opname} and {opname2}.")
+                     print_with_delay(f"Your opponent has {opname} and {opname2}.")
                      time.sleep(3)
                      if opace > 0 and optotal > 21:
                             optotal -= 10
-                            print(f"Their ace's value changes to 1 and their total is {optotal}.")
+                            _with_delayprint(f"Their ace's value changes to 1 and their total is {optotal}.")
                             opace -= 1
                             opname = False
                             time.sleep(3)
                      while round_over == False:
-                            hit_stand = input("Hit or stand? (hit/stand) ")
+                            hit_stand = input(Colors.MAGENTA + "Hit or stand? (hit/stand) ")
                             time.sleep(1)
                             if hit_stand == "hit":
                                    card_num += 1
@@ -449,18 +448,18 @@ def world2():
                                    else:
                                           name = False
                                    if name == "an ace":
-                                          print(f"You drew {name}.")
+                                          print_with_delay(Colors.GREEN + f"You drew {name}.")
                                    else:
-                                          print(f"You drew {num}")
+                                          print_with_delay(Colors.GREEN + f"You drew {num}")
                                    time.sleep(3)
                                    if ace > 0 and total > 21:
                                           total -= 10
-                                          print(f"Your ace's value changes to 1 and your total is {total}.")
+                                          print_with_delay(f"Your ace's value changes to 1 and your total is {total}.")
                                           ace -= 1
                                           time.sleep(3)
                                    if total <= 21:
                                           if card_num == 5:
-                                                 print("You win the round by holding 5 cards.")
+                                                 print_with_delay("You win the round by holding 5 cards.")
                                                  won += 1
                                                  round_over = True
                                                  break
@@ -468,25 +467,25 @@ def world2():
                                                  continue
                                    if total > 21:
                                           if total > 21 and ace == 22:
-                                                 print("Your opponent accuses you of cheating and you get thrown out of the casino.")
+                                                 print_with_delay("Your opponent accuses you of cheating and you get thrown out of the casino.")
                                                  time.sleep(3)
                                                  return "lost"
                                           else:
-                                                 print("You busted!")
+                                                 print_with_delay("You busted!")
                                                  time.sleep(3)
                                                  lost += 1
                                                  round_over = True            
                                                  break
                             else:
-                                   print(f"You stood with {total}.")
+                                   print_with_delay(f"You stood with {total}.")
                                    time.sleep(3)
                                    opdone = False
                             if ace > 11:
-                                   print("Your opponent becomes suspicious of you holding aces.")
+                                   print_with_delay("Your opponent becomes suspicious of you holding aces.")
                                    time.sleep(3)
                             while opdone == False:
                                    if 17 <= optotal <= 21 and optotal >= total:
-                                          print(f"Your opponent stood with {optotal}")
+                                          print_with_delay(f"Your opponent stood with {optotal}")
                                           time.sleep(3)
                                           opdone = True
                                           break
@@ -500,60 +499,57 @@ def world2():
                                           else:
                                                  opname = False
                                    if opname == "an ace":
-                                          print(f"Your opponent draws {opname}")
+                                          print_with_delay(f"Your opponent draws {opname}")
                                           time.sleep(3)
                                    else:
-                                          print(f"Your opponent draws {opnum}.")
+                                          print_with_delay(f"Your opponent draws {opnum}.")
                                           time.sleep(3)
                                    if opace > 0 and optotal > 21:
                                           optotal -= 10
-                                          print(f"Their ace's value changes to 1 and their total is {optotal}.")
+                                          print_with_delay(f"Their ace's value changes to 1 and their total is {optotal}.")
                                           opace -= 1
                                           time.sleep(3)
                                    if optotal > 21:
-                                          print("Your opponent has busted!")
+                                          print_with_delay("Your opponent has busted!")
                                           time.sleep(3)
                                           won += 1
                                           round_over = True
                                           break
                                    if opcard_num == 5:
-                                          print("Yor opponent wins the round by holding 5 cards.")
+                                          print_with_delay("Yor opponent wins the round by holding 5 cards.")
                                           lost += 1
                                           round_over = True
                                           break
                                    continue
                             if total <= 21 and optotal <= 21:
                                    if optotal > total:
-                                          print("Your opponent has a higher value than you, so your opponent wins the round.")
+                                          print_with_delay("Your opponent has a higher value than you, so your opponent wins the round.")
                                           time.sleep(3)
                                           lost += 1
                                           round_over = True
                                           break
                                    elif total > optotal:
-                                          print("You have a higher value than your opponent, so you win the round.")
+                                          print_with_delay(Colors.WHITE + "You have a higher value than your opponent, so you win the round.")
                                           time.sleep(3)
                                           won += 1
                                           round_over = True
                                           break
                                    else:
-                                          print("Your opponent wins due to a tie.")
+                                          print_with_delay(Colors.WHITE + "Your opponent wins due to a tie.")
                                           time.sleep(3)
                                           round_over = True
                                           lost += 1
                                           break
                      if won == 3:
-                            print("You beat your opponent.")
-                            new_game = True
+                            print_with_delay(Colors.WHITE + "You beat your opponent.")
                             time.sleep(3)
-                            break
+                            return "won"
                      elif lost == 3:
-                            print("You lost and were thrown out of the casino.")
+                            print_with_delay(Colors.WHITE + "You lost and were thrown out of the casino.")
                             time.sleep(3)
-                            new_game = True
                             return "lost"
                      else:
                             continue
-       blackjack()
 
        if world == 2:
               print_with_delay(Colors.GREEN + "You find yourself standing outside a fancy building.")
@@ -778,9 +774,11 @@ def world2():
                             time.sleep(3)
                             print_with_delay(Colors.RED + "I might have, and I might not have, if you beat me in blackjack, I'll tell you what I know.")
                             time.sleep(3)
-                            print_with_delay(Colors.WHITE + "You must beat the gambler 3 times to win, if you lose 3 times, you get kicked out.")
+                            print_with_delay(Colors.WHITE + "You must beat the gambler 3 times to win, if you lose 3 times, you'll get kicked out or the casino.")
                             time.sleep(3)
                             blackjack()
+                            if blackjack() == "won":
+                                   print_with_delay(Colors.GREEN + "The gambler tells you that the boss gambler is going to gamble away the golden toaster, and instructs you to the room he'll be in.")
               
 def world3():
        if world == 3:
