c703475012d4242825f26eaf680a3aff2906fcb4
Phanntomized <boona@wlhs.wlwv.k12.or.us>
2024-12-16 13:47:28 -0800

    poker build 3

--- poker_test.py
+++ poker_test.py
@@ -2,38 +2,39 @@ import random
 def poker():
     hand = random.randint(1,20)
     if hand == 1:
-        hand = "royal flush"
+        return "royal flush"
     else:
         hand = random.randint(1,18)
-    if hand == 1:
-        hand = "straight flush"
-    else:
-        hand = random.randint(1,16)
-    if hand == 1:
-        hand = "four of a kind"
-    else:
-        hand = random.randint(1,14)
-    if hand == 1:
-        hand = "full house"
-    else:
-        hand = random.randint(1,12)
-    if hand == 1:
-        hand = "flush"
-    else:
-        hand = random.randint(1,10)
-    if hand == 1:
-        hand = "straight"
-    else:
-        hand = random.randint(1,8)
-    if hand == 1:
-        hand = "three of a kind"
-    else:
-        hand = random.randint(1,6)
-    if hand == 1:
-        hand = "two of a kind"
-    else:
-        hand = random.randint(1,4)
-    if hand == 1:
-        hand = "pair"
-    else:
-        hand = "high card"
+        if hand == 1:
+            return "straight flush"
+        else:
+            hand = random.randint(1,16)
+            if hand == 1:
+                return "four of a kind"
+            else:
+                hand = random.randint(1,14)
+                if hand == 1:
+                    return "full house"
+                else:
+                    hand = random.randint(1,12)
+                    if hand == 1:
+                        return "flush"
+                    else:
+                        hand = random.randint(1,10)
+                        if hand == 1:
+                            return "straight"
+                        else:
+                            hand = random.randint(1,8)
+                            if hand == 1:
+                                return "three of a kind"
+                            else:
+                                hand = random.randint(1,6)
+                                if hand == 1:
+                                    return "two of a kind"
+                                else:
+                                    hand = random.randint(1,4)
+                                    if hand == 1:
+                                        return "pair"
+                                    else:
+                                        return "high card"
+print(poker())
