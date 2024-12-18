24484e4f1ab8b9f84f0aefa2f66c01798f571f08
Phanntomized <boona@wlhs.wlwv.k12.or.us>
2024-12-18 09:43:51 -0800

    poker build 9

--- poker_test.py
+++ poker_test.py
@@ -1,34 +1,34 @@
 import random
 def poker():
-    hand = random.randint(1,30)
+    hand = random.randint(1,50)
     if hand == 1:
         return "royal flush"
     else:
-        hand = random.randint(1,20)
+        hand = random.randint(1,40)
         if hand == 1:
             return "straight flush"
         else:
-            hand = random.randint(1,17)
+            hand = random.randint(1,35)
             if hand == 1:
                 return "four of a kind"
             else:
-                hand = random.randint(1,15)
+                hand = random.randint(1,30)
                 if hand == 1:
                     return "full house"
                 else:
-                    hand = random.randint(1,13)
+                    hand = random.randint(1,25)
                     if hand == 1:
                         return "flush"
                     else:
-                        hand = random.randint(1,11)
+                        hand = random.randint(1,20)
                         if hand == 1:
                             return "straight"
                         else:
-                            hand = random.randint(1,9)
+                            hand = random.randint(1,15)
                             if hand == 1:
                                 return "three of a kind"
                             else:
-                                hand = random.randint(1,7)
+                                hand = random.randint(1,10)
                                 if hand == 1:
                                     return "two of a kind"
                                 else:
